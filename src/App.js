9cf01e59e960b542155fec9e9097006ff292e28e
JuanGrajales <grajales.juanfe11@gmail.com>
2020-07-01 10:32:36 -0400

    iterations 1-3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import foods from './foods.json';
+import FoodBox from './components/Foodbox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    foodList: foods,
+    showForm: false,
+    foodName: '',
+    calories: 0,
+    image: '',
+  };
+
+  displayFood = () => {
+    let arr = this.state.foodList.map((food) => {
+      return (
+        <FoodBox
+          key={food.name}
+          name={food.name}
+          calories={food.calories}
+          image={food.image}
+        />
+      );
+    });
+    return arr;
+  };
+
+  toggleForm = () => {
+    // if (this.state.showForm === false) {
+    //   this.setState({
+    //     showForm: true,
+    //   });
+    // } else if (this.state.showForm === true) {
+    //   this.setState({
+    //     showForm: false,
+    //   });
+    // }
+    this.setState({
+      showForm: !this.state.showForm,
+    });
+  };
+
+  addFood = (event) => {
+    event.preventDefault();
+    let newFoodObj = {
+      name: this.state.foodName,
+      calories: this.state.calories,
+      image: this.state.image,
+      quantity: 0,
+    };
+
+    let foodListCopy = [...this.state.foodList];
+    foodListCopy.unshift(newFoodObj);
+
+    this.setState({
+      foodList: foodListCopy,
+      showForm: false,
+    });
+  };
+
+  handleChange = (event) => {
+    console.log(event.target.name);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  // handleChange2 = (event) => {
+  //   console.log(event.target.value);
+  //   this.setState({
+  //     calories: event.target.value,
+  //   });
+  // };
+  // handleChange3 = (event) => {
+  //   console.log(event.target.value);
+  //   this.setState({
+  //     image: event.target.value,
+  //   });
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.toggleForm}>Add Food</button>
+        {this.state.showForm ? (
+          <form onSubmit={this.addFood}>
+            <label>Food name:</label>
+            <input onChange={this.handleChange} type="text" name="name" />
+            <br />
+            <label>Calories:</label>
+            <input onChange={this.handleChange} type="number" name="calories" />
+            <br />
+            <label>Image URL:</label>
+            <input onChange={this.handleChange} type="text" name="image" />
+            <br />
+            <input type="submit" />
+          </form>
+        ) : (
+          ''
+        )}
+        {this.displayFood()}
+      </div>
+    );
+  }
 }
+
+// create a state for the array and boolean
+// create button
+// when you press a form shows
+
+// create the new food object
+// push it to the array
+// setstate
 
 export default App;
